87b9632b996f8c3bb225dc1ab21beec107305d21
Andre LaFleur <alafleur@mavenlink.com>
2020-05-21 08:56:43 -0600

    Finish setting up the login stuffs

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
   get 'welcome', to: 'sessions#welcome'
 
   root 'static_pages#index'
