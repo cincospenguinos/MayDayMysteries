45815216ddb105c070163f5ece49947b3661b16b
Andre LaFleur <alafleur@mavenlink.com>
2020-05-20 21:53:24 -0600

    Setup User with validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_21_033343) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email_address"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
