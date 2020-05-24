d24591567bbc423704e90a87f59c53b46f1a9957
Andre LaFleur <andre.lafleur@utah.edu>
2020-05-24 16:44:03 -0600

    Update show_document to use published date

--- app/javascript/packs/show_document.jsx
+++ app/javascript/packs/show_document.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import Document from "./document/document";
+import Document from "./show_document/document";
 
 function ShowDocument(props) {
   return (
